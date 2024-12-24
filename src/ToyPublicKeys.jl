453fc5295d4982df86e2de1b93b4c58629b21ab6
jan.wagner <jan.wagner@cdn77.com>
2024-12-24 11:05:20 +0100

    feature: generate random RSA key pairs

--- src/ToyPublicKeys.jl
+++ src/ToyPublicKeys.jl
@@ -1,7 +1,8 @@
 module ToyPublicKeys
 
 @enum AsymetricKeyType public_key private_key
-
-include("RSA.jl")
+include("utils/primes.jl")
+include("utils/random.jl")
+include("rsa.jl")
 export RSAKey, encrypt, decrypt
 end
