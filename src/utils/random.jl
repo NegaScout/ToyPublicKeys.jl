b2c30fa8ef51e63f32ac1e8209823e5f15a2285f
Jan Wagner <jenik.wagner@gmail.com>
2024-12-30 11:15:40 +0100

    chore: reformat code using formater

--- src/utils/random.jl
+++ src/utils/random.jl
@@ -1,4 +1,4 @@
 function random_bigint_from_range(bits::Integer)
     bits <= 0 && error("bits <= 0")
-    return rand(BigInt(2)^(bits - 1):BigInt(2)^bits)
+    return rand((BigInt(2)^(bits - 1)):(BigInt(2)^bits))
 end
