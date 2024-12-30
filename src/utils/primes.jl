db287b9656b6b555fda54656e618e094661813bf
Jan Wagner <jenik.wagner@gmail.com>
2024-12-30 20:35:32 +0100

    draft

--- src/utils/primes.jl
+++ src/utils/primes.jl
@@ -13,14 +13,14 @@ function is_probab_prime_p(n::BigInt, n_tests::Int)
     end
 end
 
-function rand_prime_for_rsa(bits::Integer, no_lcm_with=big"65537")
+function rand_prime_for_rsa(bits::Integer, no_gcd_with=big"65537")
     bits <= 0 && error("bits <= 0")
     ntest = 20
     fst = Nothing
     while true
         fst = random_bigint_from_range(bits)
         if is_probab_prime_p(fst, ntest) ∈ [:prime, :probably_prime] &&
-            gcd(fst, no_lcm_with) == 1
+            gcd(fst, no_gcd_with) == 1
             break
         end
     end
