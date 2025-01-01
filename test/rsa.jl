5ca67bc2fb4f62a4d7e80abe98c69a53703b6ab6
Jan Wagner <jenik.wagner@gmail.com>
2025-01-01 21:33:56 +0100

    refactor: move pass_trough_GMP to tests

--- test/rsa.jl
+++ test/rsa.jl
@@ -1,6 +1,23 @@
 import Random
 
 @testset "pass_trough_GMP" begin
+    function pass_trough_GMP(str::String)
+        bi = BigInt()
+        _order = 0
+        _endian = 0
+        _nails = 0
+        # https://gmplib.org/manual/Integer-Import-and-Export#index-mpz_005fimport
+        # void mpz_import (mpz_t rop, size_t count, int order, size_t size, int endian, size_t nails, const void *op)
+        msg_ = codeunits(str)
+        Base.GMP.MPZ.import!(
+            bi, length(msg_), _order, sizeof(eltype(msg_)), _endian, _nails, pointer(msg_)
+        )
+        # https://gmplib.org/manual/Integer-Import-and-Export#index-mpz_005fexport
+        # void * mpz_export (void *rop, size_t *countp, int order, size_t size, int endian, size_t nails, const mpz_t op)
+        buff = Vector{UInt8}(undef, bi.size)
+        Base.GMP.MPZ.export!(buff, bi; order=_order, nails=_nails, endian=_endian)
+        return String(buff)
+    end
     test_str = "this_is_test_str"
     @test ToyPublicKeys.pass_trough_GMP(test_str) == "this_is_test_str"
 end
