274401133d3125e9551508394e195c3984bf54b5
Jan Wagner <jenik.wagner@gmail.com>
2025-01-19 15:26:28 +0100

    utils/random: random_bigint_from_range from proper range

--- src/utils/random.jl
+++ src/utils/random.jl
@@ -5,5 +5,7 @@ Custom random big int function since core julia does not yet provide 'proper one
 """
 function random_bigint_from_range(bits::Integer)
     bits <= 0 && error("bits <= 0")
-    return rand((BigInt(2)<<(bits - 1)):(BigInt(2)<<bits))
+    lower = BigInt(1) << (bits - 1)
+    upper = (BigInt(1) << bits) - 1
+    return rand(lower:upper)
 end
