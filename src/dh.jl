5c513e727aacfe144a15225eb8a0dc19c6065138
Jan Wagner <jenik.wagner@gmail.com>
2025-02-15 19:21:39 +0100

    feature,dh: generating dh params and keypair

--- src/dh.jl
+++ src/dh.jl
@@ -1,3 +1,4 @@
+# https://www.ietf.org/rfc/rfc2631.txt
 struct DHPrivateKey
     modulus::BigInt
     public_component::BigInt
@@ -9,14 +10,30 @@ struct DHPublicKey
     public_component::BigInt
 end
 
-function DHStep(key::DHPrivateKey)
-    return Base.GMP.MPZ.powm(key.public_component, key.private_component, key.modulus)
+function DHStep(intermidiary::DHPublicKey, key::DHPrivateKey)
+    return Base.GMP.MPZ.powm(intermidiary.public_component,
+                             key.private_component,
+                             key.modulus)
 end
 
-function DHStep(intermidiary::BigInt, key::DHPrivateKey)
-    return Base.GMP.MPZ.powm(intermidiary, key.private_component, key.modulus)
+function dh_params(bits::Integer)
+    p = rand_prime_for_dh(bits)
+    q = rand_prime_for_dh(bits)
+    j = BigInt(ceil((p - 1) / q))
+    h = big"0"
+    g = big"1"
+    while true
+        h = rand(big"2":BigInt(p-2))
+        g = Base.GMP.MPZ.powm(h, j, p)
+        if g != 1
+            break
+        end
+    end
+    return (p, g)
 end
 
-function generate_DHKeyPair(modulus::BigInt, public::BigInt, private::BigInt)
-    return (DHPrivateKey(modulus, public, private), DHPublicKey(modulus, public))
+function generate_dh_key_pair(p::BigInt, g::BigInt, bits::Integer)
+    private = random_bigint_from_range(bits)
+    public = Base.GMP.MPZ.powm(g, private, p)
+    return (DHPrivateKey(p, public, private), DHPublicKey(p, public))
 end
