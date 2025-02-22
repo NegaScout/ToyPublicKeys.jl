58d3234e2bbb466898d27c92548070b5017ef282
Jan Wagner <jenik.wagner@gmail.com>
2025-02-22 23:01:37 +0100

    benchmarks: add simple dh_params benchmark

--- src/ToyPublicKeys.jl
+++ src/ToyPublicKeys.jl
@@ -6,5 +6,5 @@ include("utils/number_theory.jl")
 include("padding/pkcs_1_v1_5.jl")
 include("rsa.jl")
 include("dh.jl")
-export RSAKey, RSAPrivateKey, RSAPublicKey, generate_rsa_key_pair, encrypt, decrypt, sign, verify_signature
+export RSAKey, RSAPrivateKey, RSAPublicKey, generate_rsa_key_pair, encrypt, decrypt, sign, verify_signature, dh_params
 end
