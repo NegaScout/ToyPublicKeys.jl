e2f5612aa1d84c6115d82a133ef87076f2a9a454
Jan Wagner <jenik.wagner@gmail.com>
2025-02-22 20:12:33 +0100

    refactor: dh_params more according to RFC2631

--- test/dh.jl
+++ test/dh.jl
@@ -1,10 +1,9 @@
 
 @testset "dh basics" begin
     Random.seed!(42)
-    bits = 128
-    p, g = ToyPublicKeys.dh_params(bits)
-    a_private_key, a_public_key = ToyPublicKeys.generate_dh_key_pair(p, g, bits)
-    b_private_key, b_public_key = ToyPublicKeys.generate_dh_key_pair(p, g, bits)
+    g, p, q, SEED, counter = ToyPublicKeys.dh_params(1024, 160)
+    a_private_key, a_public_key = ToyPublicKeys.generate_dh_key_pair(p, g, 1024)
+    b_private_key, b_public_key = ToyPublicKeys.generate_dh_key_pair(p, g, 1024)
     a_shared_s = ToyPublicKeys.DHStep(b_public_key, a_private_key)
     b_shared_s = ToyPublicKeys.DHStep(a_public_key, b_private_key)
     @test a_shared_s == b_shared_s
