66f6fa694bb99880413ae0dd398c3b4b68cee8fa
Jan Wagner <jenik.wagner@gmail.com>
2025-03-15 21:01:22 +0100

    buffer/integer import/export fixes

--- src/utils/string.jl
+++ src/utils/string.jl
@@ -1,8 +1,15 @@
-function I2OSP(x::BigInt)
-    return I2OSP(x, Base.GMP.MPZ.sizeinbase(x, 16))
+function s_to_os(buf::String)
+    _buf = buf |> uppercase
+    it = Iterators.Stateful(_buf)
+    part = Base.Iterators.partition(it, 2)
+    return join(map(join, part), ':')
 end
 
-function I2OSP(x::BigInt, xLen::Integer)
+function i2osp(x::BigInt)
+    return i2osp(x, Base.GMP.MPZ.sizeinbase(x, 16))
+end
+
+function i2osp(x::BigInt, xLen::Integer)
     xLen |> isodd && (xLen += 1)
     base_size = Base.GMP.MPZ.sizeinbase(x, 16)
     base_size > xLen && throw(error("integer too big for xLen"))
@@ -10,17 +17,41 @@ function I2OSP(x::BigInt, xLen::Integer)
     fill!(buf, '0')
     buf_ptr = pointer(buf)
     Base.GMP.MPZ.get_str!(buf_ptr + xLen - base_size, 16, x)
-    _buf = String(buf) |> uppercase
-    it = Iterators.Stateful(_buf)
-    part = Base.Iterators.partition(it, 2)
-    return join(map(join, part), ':')
+    return s_to_os(buf |> String)
 end
 
-function OS2IP(x::String)
+function os2ip(x::String)
     # use Cstring instead..?
     buf = replace(x, ":" => "") |> lowercase |> Vector{UInt8}
     push!(buf, 0)
     target = BigInt(0)
     Base.GMP.MPZ.set_str!(target, pointer(buf), 16) == 0 || throw(error("string not valid base 16"))
     return target
+end
+
+function I2OSP(x::BigInt, xLen::Integer)
+    _order = 0
+    _endian = 0
+    _nails = 0
+    n = (Base.GMP.MPZ.sizeinbase(x, 2) / 8) |> ceil |> Integer
+    ret = zeros(UInt8, n)
+    Base.GMP.MPZ.export!(ret, x; order=_order, nails=_nails, endian=_endian)
+    ret_len = ret |> length
+    if ret_len < xLen
+        ret = vcat(zeros(UInt8, xLen - ret_len), ret)
+    elseif ret_len > xLen
+        error("ret_len > xLen") |> throw
+    end
+    return ret
+end
+
+function OS2IP(x::Vector{UInt8})
+    bi = BigInt()
+    _order = 0
+    _endian = 0
+    _nails = 0
+    Base.GMP.MPZ.import!(
+        bi, length(x), _order, sizeof(eltype(x)), _endian, _nails, pointer(x)
+    )
+    return bi
 end
