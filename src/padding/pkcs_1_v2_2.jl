0c2f981d41917cb183f3841ec5ab2055d4cab72a
Jan Wagner <jenik.wagner@gmail.com>
2025-03-15 21:01:31 +0100

    padding fixes

--- src/padding/pkcs_1_v2_2.jl
+++ src/padding/pkcs_1_v2_2.jl
@@ -1,10 +1,9 @@
 function pad(::pkcs1_v2_2_t,
-             msg::Union{AbstractString, AbstractVector},
+             msg::Vector{UInt8},
              key::RSAKey;
-             label="",
+             label=UInt8[],
              MGF=ToyPublicKeys.MGF1,
              hash=SHA.sha1)
-    msg = msg |> Vector{UInt8}
     k = (Base.GMP.MPZ.sizeinbase(key.modulus, 2)/8) |> ceil |> Integer
     lHash = hash(label)
     hLen = lHash |> length
@@ -25,15 +24,16 @@ function pad(::pkcs1_v2_2_t,
 end
 
 function unpad(::pkcs1_v2_2_t,
-               msg::Union{AbstractString, AbstractVector},
+               msg::Vector{UInt8},
                key::RSAKey;
-               label="",
+               label=UInt8[],
                MGF=ToyPublicKeys.MGF1,
                hash=SHA.sha1)
     k = (Base.GMP.MPZ.sizeinbase(key.modulus, 2)/8) |> ceil |> Integer
     lHash = hash(label)
     hLen = lHash |> length
     Y = msg[1]
+    Y != 0 && error("Y != 0") |> throw
     maskedSeed = msg[2:end][1:hLen]
     DBLen = k - hLen - 1
     maskedDB = msg[2:end][hLen + 1:end]
@@ -42,21 +42,27 @@ function unpad(::pkcs1_v2_2_t,
     dbMask = MGF(seed, k - hLen - 1)
     DB = maskedDB .⊻ dbMask
     lHashPrime = DB[1:hLen]
-    PSLen = findfirst(Vector{UInt8}([1]), DB)
-    PSIndex = (PSLen |> first) - 1
-    PS = DB[hLen + 1:PSIndex]
-    X = DB[PSIndex + 1]
-    M = DB[PSIndex + 2:end]
+    lHashPrime != lHash && error("lHashPrime != lHash")
+    PSIndexInView = findfirst(a -> a==1, DB[hLen + 1:end])
+    isnothing(PSIndexInView) && error("not EME-OAEP encoded or malformed")
+    PSIndex = PSIndexInView + hLen
+    PS = DB[hLen + 1:PSIndex - 1]
+    any(PS .!= 0) && error("PS should be zero filled vector") |> throw
+    X = DB[PSIndex]
+    X != 1 && error("X != 1") |> throw
+    M = DB[PSIndex + 1:end]
     return M
 end
 
 function MGF1(mgfSeed::Vector{UInt8}, maskLen:: Integer; hash = SHA.sha1)
-    hLen = hash("") |> length
+    hLen = hash(UInt8[]) |> length
     maskLen >= (2 << 32) && error("mask too long") |> throw
-    T = Vector{UInt8}()
+    T = zeros(UInt8, 0)
     for counter in big"0":BigInt((ceil(maskLen / hLen) - 1))
-        C = I2OSP(counter, 4) |> Vector{UInt8}
-        T = vcat(T, hash(vcat(mgfSeed, C) |> String))
+        C = I2OSP(counter, 4)
+        _T = vcat(mgfSeed, C)
+        __T = hash(_T)
+        T = vcat(T, __T)
     end
     return T[1:maskLen]
 end
