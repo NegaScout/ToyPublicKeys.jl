cce0701be1969b3e35af41dc9facedbcc539d1ab
NegaScout <wagneja4@fel.cvut.cz>
2025-03-27 21:57:10 +0100

    removing pkcs 1 v1.5 and replacing relevant parts with pkcs#1 v2.2 backwards compatible implementation

--- src/ToyPublicKeys.jl
+++ src/ToyPublicKeys.jl
@@ -6,7 +6,6 @@ include("utils/number_theory.jl")
 include("utils/string.jl")
 include("rsa.jl")
 include("pkcs1_v2_2.jl")
-include("padding/pkcs_1_v1_5.jl")
 include("dh.jl")
 export RSAKey, RSAPrivateKey, RSAPublicKey, generate_rsa_key_pair, encrypt, decrypt, sign, verify_signature, dh_params
 end
